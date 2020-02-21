bfa08064657590d20de88a9a51d04732b3aac44a
kuyewb <kuyewb@163.com>
2020-02-21 19:54:58 +0800

    20200221
    20200221

---
+++ data/czml.js
@@ -0,0 +1,29 @@
+var czml = [
+    {
+        "id" : "documnet",
+        "name" : "box",
+        "version" : "1.0"
+    },{
+        "id" : "shape2",
+        "name" : "Red box with black outline",
+        "position" : {
+            "cartographicDegrees" : [117,27,30000.0]
+        },
+        "box" : {
+            "dimensions" : {
+                "cartesian" : [40000,30000,50000]
+            },
+            "material" : {
+                "solidColor" : {
+                    "color" : {
+                        "rgba" : [255,0,0,128]
+                    }
+                }
+            },
+            "outline" : true,
+            "outlineColor" : {
+                "rgba" : [0,0,0,255]
+            }
+        }
+    }
+];
